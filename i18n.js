9fab047409f9f2b5bddc4a18a0616f331cc6e0d9
ShaYri12 <sharjeelb60@gmail.com>
2025-02-23 04:40:45 +0500

    fix

--- i18n.js
+++ i18n.js
@@ -2,43 +2,14 @@
 
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import HttpBackend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
 
-i18n
-  .use(HttpBackend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: "en",
-    supportedLngs: ["en", "pt", "ru"],
-    ignoreJSONStructure: false,
-    ns: ["translation"], 
-    defaultNS: "translation",  
-    debug: process.env.NODE_ENV !== "production",
-    preload: ["en", "pt", "ru"], 
-
-    interpolation: {
-      escapeValue: false,
-    },
-
-    backend: {
-      loadPath: "/locales/{{lng}}/translation.json",
-      cache: true,  
-    },
-    
-
-    detection: {
-      order: ["localStorage", "path", "navigator"],
-      caches: ["localStorage"],
-    },
-
-    lng:
-      typeof window !== "undefined"
-        ? localStorage.getItem("selectedLanguage") || "en"
-        : "en",
-  });
-
-
+i18n.use(initReactI18next).init({
+  lng: "en", // Hardcode to English for testing
+  fallbackLng: "en",
+  supportedLngs: ["en", "pt-BR", "ru"],
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
