fc5af2958633726140eb6b6c5dfd97ebb516bbfa
ShaYri12 <sharjeelb60@gmail.com>
2025-02-23 04:43:38 +0500

    deployment fix

--- i18n.js
+++ i18n.js
@@ -2,14 +2,40 @@
 
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import HttpBackend from "i18next-http-backend"; // Ensure this is included for loading translations
+import LanguageDetector from "i18next-browser-languagedetector"; // Ensure this is included for language detection
 
-i18n.use(initReactI18next).init({
-  lng: "en", // Hardcode to English for testing
-  fallbackLng: "en",
-  supportedLngs: ["en", "pt-BR", "ru"],
-  interpolation: {
-    escapeValue: false,
-  },
-});
+i18n
+  .use(HttpBackend) // Use the backend to load translations
+  .use(LanguageDetector) // Use the language detector
+  .use(initReactI18next) // Pass i18n down to react-i18next
+  .init({
+    fallbackLng: "en", // Default language
+    supportedLngs: ["en", "pt-BR", "ru"], // Supported languages
+    ignoreJSONStructure: false,
+    ns: ["translation"], // Namespaces for translations
+    defaultNS: "translation", // Default namespace
+    debug: process.env.NODE_ENV !== "production", // Enable debug mode in development
+    preload: ["en", "pt-BR", "ru"], // Preload languages
+
+    interpolation: {
+      escapeValue: false, // React already does escaping
+    },
+
+    backend: {
+      loadPath: "/locales/{{lng}}/translation.json", // Path to your translation files
+      cache: true, // Cache translations
+    },
+
+    detection: {
+      order: ["localStorage", "path", "navigator"], // Order of language detection
+      caches: ["localStorage"], // Cache detected language
+    },
+
+    lng:
+      typeof window !== "undefined"
+        ? localStorage.getItem("selectedLanguage") || "en"
+        : "en", // Set initial language
+  });
 
 export default i18n;
