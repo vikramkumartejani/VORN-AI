79679ab59e0f9f4c43f6e8d9cc12ce604ce51b30
vikram kumar <vikramkumar8t8@gmail.com>
2025-02-24 02:20:56 +0500

    ai utility page

--- app/components/AIUtility/AIUtilityServices.js
+++ app/components/AIUtility/AIUtilityServices.js
@@ -6,7 +6,7 @@ const AIUtilityServices = () => {
     <div className='py-[92px] w-full'>
         <div className='max-w-[721px] mx-auto w-full flex items-center flex-col justify-center'>
             <h2 className='text-[40px] leading-[60px] font-bold text-center text-white mb-[35px]'><span className='text-[#9442ED]'>AI Utility Services</span> for Seamless and Profitable Automation</h2>
-            <Image src='/assets/ai-utility-services.svg' alt='ai-utility-services' width={721} height={583} />
+            <Image src='/assets/ai-utility-services.webp' alt='ai-utility-services' width={721} height={583} />
         </div>
     </div>
   )
