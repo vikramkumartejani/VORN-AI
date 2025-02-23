109ef7600bd22e1d6d78ca573ed14d32930663af
vikram kumar <vikramkumar8t8@gmail.com>
2025-02-23 22:43:58 +0500

    ai page

---
+++ app/components/AIUtility/AIUtilityServices.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import React from 'react'
+
+const AIUtilityServices = () => {
+  return (
+    <div className='py-[92px] w-full'>
+        <div className='max-w-[721px] mx-auto w-full flex items-center flex-col justify-center'>
+            <h2 className='text-[40px] leading-[60px] font-bold text-center text-white mb-[35px]'><span className='text-[#9442ED]'>AI Utility Services</span> for Seamless and Profitable Automation</h2>
+            <Image src='/assets/ai-utility-services.svg' alt='ai-utility-services' width={721} height={583} />
+        </div>
+    </div>
+  )
+}
+
+export default AIUtilityServices
