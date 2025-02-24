807c1e220668e1f8b7178c4570e240fec111ce43
vikram kumar <vikramkumar8t8@gmail.com>
2025-02-24 20:23:06 +0500

    de-pin page

---
+++ app/components/DePIN/PoweringVORNAI.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PoweringVORNAI = () => {
+  return (
+    <div className='py-[140px] bg-[#020103] w-full'>
+        <h1 className='max-w-[650px] mx-auto w-full text-[40px] leading-[60px] font-bold text-white text-center'><span className='text-[#9442ED]'>Powering VORN AI</span> with Decentralized Infrastructure</h1>
+        <div className='mt-[161px]'>
+
+        </div>
+    </div>
+  )
+}
+
+export default PoweringVORNAI
