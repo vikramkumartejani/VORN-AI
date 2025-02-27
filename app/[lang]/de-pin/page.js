16c65d7a60ff81b4effa18798b31bc1b925acbac
vikram kumar <vikramkumar8t8@gmail.com>
2025-02-27 20:22:57 +0500

    DePIN responsive

--- app/[lang]/de-pin/page.js
+++ app/[lang]/de-pin/page.js
@@ -17,12 +17,12 @@ const DePIN = () => {
       <Header />
 
       {/* Hero Section */}
-      <div className="relative flex flex-col items-center justify-center text-center h-[683px] px-6">
+      <div className="relative flex flex-col items-center justify-center text-center h-[683px] px-4 md:px-6">
         {/* Background Video */}
         <div className="absolute inset-0 overflow-hidden z-0">
           <div className="absolute inset-0 bg-[#02010380]/50 z-10"></div>
           <video
-            className="absolute w-full h-full object-contain"
+            className="absolute w-full h-full object-cover"
             autoPlay
             loop
             muted
@@ -34,7 +34,7 @@ const DePIN = () => {
 
         {/* Hero Content */}
         <div className="max-w-[850px] mx-auto z-20 relative">
-          <h1 className="text-4xl md:text-[54px] leading-[74px] font-bold text-white">
+          <h1 className="text-[40px] md:text-[54px] leading-[48px] md:leading-[74px] font-bold text-white">
             {t("depin.title")}
           </h1>
           <p className="max-w-[665px] mx-auto text-[18px] leading-[30px] font-normal font-poppins text-white mt-[25px]">
--- app/components/DePIN/PoweringVORNAI.js
+++ app/components/DePIN/PoweringVORNAI.js
@@ -1,12 +1,47 @@
+import Image from 'next/image'
 import React from 'react'
 
 const PoweringVORNAI = () => {
   return (
-    <div className='py-[140px] bg-[#020103] w-full'>
-        <h1 className='max-w-[650px] mx-auto w-full text-[40px] leading-[60px] font-bold text-white text-center'><span className='text-[#9442ED]'>Powering VORN AI</span> with Decentralized Infrastructure</h1>
-        <div className='mt-[161px]'>
+    <div className='pt-[99px] pb-[200px] lg:py-[140px] bg-[#020103] w-full px-6 sm:px-10'>
+        <h1 className='px-10 max-w-[650px] mx-auto w-full text-[32px] md:text-[40px] leading-[38px] md:leading-[60px] font-bold text-white text-center'><span className='text-[#9442ED]'>Powering VORN AI</span> with Decentralized Infrastructure</h1>
+        <div className='mt-[161px] lg:block hidden'>
 
         </div>
+        <div className='mt-[50px] lg:hidden block'>
+          <div className='flex items-center flex-col md:flex-row gap-6'>
+            <Image src='/assets/de-pin/decentralized-ai-compute-nodes.png' alt='image' width={557} height={437} className='flex-1' />
+            <div className='text-center md:text-left flex-1'>
+              <h2 className='text-white text-[28px] leading-[33.6px] font-bold mb-5'>Decentralized AI Compute Nodes</h2>
+              <p className='text-white/80 text-[18px] leading-[27px] font-normal font-poppins'>Share your computing power for AI tasks, with AI optimizing workloads. Earn VRN tokens based on your contribution.</p>
+            </div>
+          </div>
+
+          <div className='mt-[50px] flex items-center flex-col md:flex-row-reverse gap-6'>
+            <Image src='/assets/de-pin/decentralized-cloud-storage.png' alt='image' width={557} height={437} className='flex-1' />
+            <div className='text-center md:text-left flex-1'>
+              <h2 className='text-white text-[28px] leading-[33.6px] font-bold mb-5'>Decentralized Cloud Storage</h2>
+              <p className='text-white/80 text-[18px] leading-[27px] font-normal font-poppins'>Provide secure, decentralized storage with AI handling encryption and retrieval. Get rewarded in VRN tokens.</p>
+            </div>
+          </div>
+
+          <div className='mt-[50px] flex items-center flex-col md:flex-row gap-6'>
+            <Image src='/assets/de-pin/decentralized-energy-grid.png' alt='image' width={557} height={437} className='flex-1' />
+            <div className='text-center md:text-left flex-1'>
+              <h2 className='text-white text-[28px] leading-[33.6px] font-bold mb-5'>Decentralized Energy Grid</h2>
+              <p className='text-white/80 text-[18px] leading-[27px] font-normal font-poppins'>Tokenize and trade renewable energy while AI balances grid loads. Earn VRN tokens for contributing energy resources.</p>
+            </div>
+          </div>
+
+          <div className='mt-[50px] flex items-center flex-col md:flex-row-reverse gap-6'>
+            <Image src='/assets/de-pin/iot-smart-cities-integration.png' alt='image' width={557} height={437} className='flex-1' />
+            <div className='text-center md:text-left flex-1'>
+              <h2 className='text-white text-[28px] leading-[33.6px] font-bold mb-5'>IoT & Smart Cities Integration</h2>
+              <p className='text-white/80 text-[18px] leading-[27px] font-normal font-poppins'>AI processes real-time IoT data for smart applications like traffic and environment monitoring. Device operators earn VRN tokens.</p>
+            </div>
+          </div>
+        </div>
+      
     </div>
   )
 }
