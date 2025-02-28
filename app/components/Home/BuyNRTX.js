d27e4704ae4b4d646a0fcc0dbd7a48317d44a32f
vikram kumar <vikramkumar8t8@gmail.com>
2025-02-28 16:51:33 +0500

    BuyNRTX section

--- app/components/Home/BuyNRTX.js
+++ app/components/Home/BuyNRTX.js
@@ -51,10 +51,48 @@ const BuyNRTX = () => {
 
         {/* Steps Container */}
         <div className="mt-11 max-w-[1236px] w-full mx-auto grid grid-cols-1 md:grid-cols-2 lg:flex gap-5">
+          {/* Desktop Design */}
           {steps.map((item, index) => (
             <div
               key={index}
-              className={`lg:${item.marginTop} relative lg:w-[399px] lg:h-[450px] xl:h-[414px] rounded-[20px] p-[1px] transition-shadow duration-300 hover:shadow-[0px_0px_25px_0px_#8B5CF6]`}
+              className={`hidden lg:block relative lg:w-[399px] lg:h-[450px] xl:h-[414px] rounded-[20px] p-[1px] transition-shadow duration-300 hover:shadow-[0px_0px_25px_0px_#8B5CF6] ${item.marginTop}`}
+              style={{
+                background:
+                  "conic-gradient(from 180deg at 50% 50%, #959595 0deg, #2F2F2F 36.1deg, #2F2F2F 77.33deg, #2F2F2F 131.17deg, #2F2F2F 163.25deg, #959595 180.4deg, #2F2F2F 196.46deg, #2F2F2F 238.85deg, #2F2F2F 299.56deg, #959595 360deg)",
+              }}
+            >
+              <div
+                className="rounded-[20px] p-[25px] flex items-center flex-col w-full h-full transition-shadow duration-300 hover:shadow-[0px_0px_50px_0px_#8B5CF680_inset] backdrop-blur-[10px]"
+                style={{
+                  background:
+                    "linear-gradient(180deg, #000000 0%, #371866 100%)",
+                  boxShadow: "0px 0px 50px 0px #8B5CF680 inset",
+                }}
+              >
+                <h2 className="text-[24px] leading-[28.8px] font-semibold text-white mb-5">
+                  {t(item.step)}
+                </h2>
+                <h2 className="text-[24px] leading-[28.8px] font-bold text-white mb-6">
+                  {t(item.title)}
+                </h2>
+                <Image
+                  src={item.image}
+                  alt={item.alt}
+                  width={104}
+                  height={104}
+                  className="sm:w-[104px] sm:h-[104px] w-[90px] h-[90px]"
+                />
+                <p className="mt-[26px] text-white font-normal font-poppins text-[16px] leading-[24px]">
+                  {t(item.description)}
+                </p>
+              </div>
+            </div>
+          ))}
+          {/* Mobile Design */}
+          {steps.map((item, index) => (
+            <div
+              key={index}
+              className={`lg:hidden block relative lg:w-[399px] lg:h-[450px] xl:h-[414px] rounded-[20px] p-[1px] transition-shadow duration-300 hover:shadow-[0px_0px_25px_0px_#8B5CF6]`}
               style={{
                 background:
                   "conic-gradient(from 180deg at 50% 50%, #959595 0deg, #2F2F2F 36.1deg, #2F2F2F 77.33deg, #2F2F2F 131.17deg, #2F2F2F 163.25deg, #959595 180.4deg, #2F2F2F 196.46deg, #2F2F2F 238.85deg, #2F2F2F 299.56deg, #959595 360deg)",
